027fc1ddf5f3e4127a6a1a4ec75755ca7536e483
Valeria Zuluaga Alzate <141469520+ValeriaZulu@users.noreply.github.com>
2026-09-10 09:49:25 -0500

    Change model filename from modelo-reg.pkl to modelo-reg1.pkl

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 
 #Cargamos el modelo
 import pickle
-filename = 'modelo-reg.pkl'
+filename = 'modelo-reg1.pkl'
 modelo, min_max_scaler, variables = pickle.load(open(filename, 'rb'))
 
 #modelo #me muestra qué modelo escogí
